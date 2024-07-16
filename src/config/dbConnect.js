aa19d4fa8958e35bcbb8bf6c987f38dee8460419
Ramon Castro Barbosa <ramonsengineer.oficial@gmail.com>
2024-07-15 23:25:27 -0300

    fix update all

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,19 +3,7 @@ import dotenv from "dotenv";
 
 //usado para estabelecer a conexeção com o bd mongo atlas e meu .env
 
-// async function conectaDatabase(){
-//   mongoose.connect(process.env.DB_CONNECT_STRING);
-
-//   return mongoose.connection
-// }
-
-// export default conectaDatabase;
-
-dotenv.config();
-
-const DB_CONNECT_STRING = process.env.DB_CONNECT_STRING;
-
-mongoose.connect(DB_CONNECT_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.DB_CONNECT_STRING);
 let conectaDatabase = mongoose.connection;
+
 export default conectaDatabase;
--- src/routes/AuthorRoutes.js
+++ src/routes/AuthorRoutes.js
@@ -3,13 +3,6 @@ import AuthorController from "../controllers/AuthorController.js";
 
 const routes = express.Router();
 
-// router.get("/autores", AuthorController.listarAutores);
-// router.get("/autores/:id", AuthorController.listarAutorPorId);
-// router.post("/autores", AuthorController.cadastrarAutores);
-// router.put("/autores/:id", AuthorController.atualizarAutorPorId);
-// router.delete("/autores/:id", AuthorController.deletarAutor);
-
-
 routes
   .get("/autores", AuthorController.listarAutores)
   .get("/autores/:id", AuthorController.listarAutorPorId)
